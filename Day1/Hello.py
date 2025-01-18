7c5b25742353acdd7a91c3fd7869803486f8e94a
MFF0 <alfawazmeshal@gmail.com>
2025-01-18 19:37:17 +0300

    Finished Day1

--- Day1/Hello.py
+++ Day1/Hello.py
@@ -1,1 +1,18 @@
+#Task 1
 print("Hello, World!")
+
+#Task 2
+print("Hello World!\nHello World!\nHello World!")
+
+#Task3
+name = input("My name is: ")
+print(f"Hello, {name}")
+
+#Task 4
+nameLen = len(name)
+print(f"Its length is: {nameLen}")
+
+#Task 5
+city = input("What city did you geow up in? ")
+pet = input("Name a pet: ")
+print(f"Your bands name is {city+pet}")
