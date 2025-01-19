5a046a386f116adfc1105a09201cf83090cc0d9f
MFF0 <alfawazmeshal@gmail.com>
2025-01-20 00:52:23 +0300

    Day2

---
+++ Day2/Day2.py
@@ -0,0 +1,11 @@
+#Task1
+print(type(12.4))
+print(type("hello"))
+print(type(12))
+print(type(True))
+
+#Task2
+print("Number of letters in your name: " + str(len(input("Enter your name"))))
+
+#Task3
+print(3 * (3 + 3) / 3 - 3)
